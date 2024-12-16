1757e42925303af303b67dd1068159f96a57afe3
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-12-16 21:25:11 +0330

    build a custom user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,76 @@
+import uuid
+from django.utils import timezone
+
 from django.db import models
-from django.contrib.auth.models import UserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        if not username:
+            raise ValueError('Users must have an username')
+
+        if not password:
+            raise ValueError('Users must have a password')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email()
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            email=self.normalize_email(),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    full_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def get_full_name(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'user'
+        ordering = ['-date_joined']
+        get_latest_by = 'date_joined'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def __str__(self):
+        return self.username
