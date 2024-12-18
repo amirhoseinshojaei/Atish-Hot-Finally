78cefc11465391a47c784a189e0980f51cf0ed07
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-12-18 17:37:38 +0330

    Add custom admin configurations for User, Categories, and Products models
    - Registered custom `UserAdmin`, `CategoriesAdmin`, and `ProductsAdmin` classes to enhance admin interface.
    - Added `list_display`, `list_filter`, `search_fields`, and `ordering` for better navigation and search.
    - Configured `readonly_fields` to restrict editing on certain fields (e.g., `date_joined`, `last_login`).
    - Implemented custom permissions for adding, deleting, and changing objects based on user roles (superuser/staff).
    - Integrated `ProductsImageInline` for managing product images directly within the product form.
    - Added prepopulated slug field for `Categories` based on the `name` field.
    - Enabled `list_editable` for key fields like `is_active`, `is_sale`, `is_suggestion` in `Products` admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,125 @@
 from django.contrib import admin
+from .models import User, Categories, Products, ProductsImage
+
 
 # Register your models here.
+
+class ProductsImageInline(admin.TabularInline):
+    model = ProductsImage
+    extra = 1
+
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'username',
+        'email',
+        'is_active',
+        'is_admin',
+        'is_staff',
+        'is_superuser',
+        'date_joined',
+        'last_login',
+
+    )
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('-date_joined',)
+    readonly_fields = ('date_joined', 'last_login')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_module_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+        'created_at',
+        'updated_at',
+    )
+
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'slug')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    prepopulated_fields = {'slug': ('name',)}
+
+    list_editable = ('name', 'slug')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_module_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'get_thumbnail'
+        'slug',
+        'price',
+        'stock',
+        'is_active',
+        'is_sale',
+        'new_price',
+        'get_price_in_toman'
+        'is_suggestion',
+        'category',
+        'created_at',
+        'updated_at',
+    )
+
+    inlines = [ProductsImageInline]
+
+    list_filter = ('is_active', 'is_sale', 'is_suggestion', 'category')
+    search_fields = ('name', 'slug')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    list_editable = ('is_active', 'is_sale', 'is_suggestion', 'category', 'new_price', 'price')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_module_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+
