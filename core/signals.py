bb6b9e933c8687df836f03611a5b15dbc3086ac6
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-12-21 17:47:12 +0330

    Add signal to restore product stock when order is canceled, Enhance VendorProfile creation and updating logic in Vendor model signals , Add signal to create and update Profile on User model save , Add signal to save user profile after user save

--- core/signals.py
+++ core/signals.py
@@ -25,7 +25,58 @@ def activate_product(sender, instance, **kwargs):
         instance.save()
 
 
+@receiver(post_save, sender=Orders)
+def restore_stock_on_cancellation(sender, instance, **kwargs):
+    # بررسی اینکه وضعیت به "Cancelled" تغییر کرده است
+    if instance.status == 'Canceled':
+        # پیمایش همه محصولات مرتبط با سفارش
+        order_items = OrderItem.objects.filter(order=instance)
+        for order_item in order_items:
+            product = order_item.product
+            product.stock += order_item.quantity  # بازگرداندن موجودی
+            product.save()  # ذخیره تغییرات
+
+
 @receiver(post_save, sender=Vendors)
 def create_vendor_profile(sender, instance, created, **kwargs):
     if created:  # بررسی اینکه آیا رکورد جدید ایجاد شده است
-        VendorProfile.objects.create(vendor=instance)
+        VendorProfile.objects.create(
+            vendor=instance,
+            first_name=instance.first_name,
+            last_name=instance.last_name,
+            vendor_code=instance.code,
+            city=instance.city,
+            address=instance.address,
+            phone_number=instance.phone_number,
+            profit=instance.get_total_profit(),  # متد باید فراخوانی شود
+        )
+    else:
+        vendor_profile = VendorProfile.objects.get(vendor=instance)  # دریافت پروفایل موجود
+        vendor_profile.first_name = instance.first_name  # به‌روزرسانی فیلدها
+        vendor_profile.last_name = instance.last_name
+        vendor_profile.city = instance.city
+        vendor_profile.address = instance.address
+        vendor_profile.phone_number = instance.phone_number
+        vendor_profile.profit = instance.get_total_profit()  # متد را فراخوانی کنید
+        vendor_profile.save()
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance,
+            full_name=instance.full_name,  # کپی کردن نام کامل از یوزر
+            email=instance.email,  # کپی کردن ایمیل از یوزر
+            phone_number=instance.phone_number,
+        )
+    else:
+        profile = instance.profile
+        profile.full_name = instance.full_name  # به‌روزرسانی نام کامل
+        profile.email = instance.email  # به‌روزرسانی ایمیل
+        profile.save()
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
